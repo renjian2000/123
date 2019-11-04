aa908316e9e1c6b894eea68ac6ef92265df78f6d
hollowrj <2044722801@qq.com>
2019-11-04 13:40:06 +0800

    123123

---
+++ src/views/children.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{num}}
+    <button @click="childrenClick">点击向父组件传值</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["num"],
+  methods: {
+    childrenClick() {
+      // this.$emit("child", this.num);
+      this.$bus.$emit("ziji", { a: 1, b: 2 });
+    }
+  }
+};
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    this is home page
+    <button @click="btnClick">点击传参</button>
+    {{num}}
+    <Children :num="num" @child="children" />
+  </div>
+</template>
+<script>
+import Children from "./children";
+export default {
+  components: {
+    Children
+  },
+  data() {
+    return {
+      num: 1
+    };
+  },
+  created() {
+    console.log();
+    this.$bus.$on("ziji", obj => {
+      console.log(obj);
+    });
+  },
+  methods: {
+    btnClick() {
+      this.num++;
+    },
+    children(num) {
+      this.num = num;
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
